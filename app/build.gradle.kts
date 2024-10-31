c74d398c5398e4d70aeef2ecd70c94d5cee55e7e
Roman Pytkov <roman.pytkov@mail.ru>
2024-10-31 16:27:52 +0300

    Модель, обновление зависимостей

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.github.nullptroma.wallenc"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.github.nullptroma.wallenc"
